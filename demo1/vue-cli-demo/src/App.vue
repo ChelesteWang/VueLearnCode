f4ccbc7a3ee1565bbdf86a6514b4d2c4da9808fb
wxy <1581446178@qq.com>
2020-03-02 12:50:05 +0800

    完成demo1

--- demo1/vue-cli-demo/src/App.vue
+++ demo1/vue-cli-demo/src/App.vue
@@ -6,9 +6,8 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
+  name: 'app',
   components: {
     HelloWorld
   }
@@ -16,12 +15,7 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  /* margin-top: 60px; */
+body {
+  margin: 0;
 }
 </style>
